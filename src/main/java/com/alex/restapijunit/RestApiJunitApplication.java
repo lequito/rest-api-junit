8ea413b4161982fdf5fc758d739dc63ea188fd29
lequito <=>
2022-08-16 01:06:42 -0300

    commit inicial

---
+++ src/main/java/com/alex/restapijunit/RestApiJunitApplication.java
@@ -0,0 +1,13 @@
+package com.alex.restapijunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/restapijunit/RestApiJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.restapijunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
